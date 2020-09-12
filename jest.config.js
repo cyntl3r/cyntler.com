3b84a49c34cebfe7326f9de8dec489b36cbbf1b3
cyntl3r <damian@cyntler.com>
2020-09-12 13:49:36 +0200

    Add @testing-library/react to testing React

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 /*global module*/
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
 };
--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,5 +1,12 @@
-describe('<App /> component', () => {
-  test('example', () => {
-    expect(1).toBe(1);
-  });
+import React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
+import { App } from './App';
+
+afterEach(() => {
+  cleanup();
+});
+
+test('is wrapper', () => {
+  render(<App />);
+  expect(screen.getByText('cyntler.com')).toBeDefined();
 });
