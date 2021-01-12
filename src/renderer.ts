ce38690a7d0023c737ffaed2f113d73aa648deb0
cyntler <damian@cyntler.com>
2021-01-12 15:27:22 +0100

    console.log

--- src/renderer.ts
+++ src/renderer.ts
@@ -5,6 +5,8 @@
 import { ReactElement } from 'react';
 import { hydrate, render } from 'react-dom';
 
+console.log(1);
+
 export const renderer = (element: ReactElement, container: Element) => {
   if (container.hasChildNodes()) {
     hydrate(element, container);
