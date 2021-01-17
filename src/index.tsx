b36dfdb5daf84d044b3c2c8ecb91a4323b266246
cyntler <damian@cyntler.com>
2021-01-17 14:08:50 +0100

    Use of locale context

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,19 @@ import { GlobalStyles } from './globalStyles';
 import { App } from './components/App/App';
 import { renderer } from './renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { StrictMode, Suspense } from 'react';
+import { StrictMode } from 'react';
+import { LocaleContextProvider } from './contexts/localeContext';
 
 const appElement = document.querySelector('main');
 
 renderer(
   <StrictMode>
-    <Suspense fallback={<div />}>
+    <LocaleContextProvider>
       <Router>
         <GlobalStyles />
         <App />
       </Router>
-    </Suspense>
+    </LocaleContextProvider>
   </StrictMode>,
   appElement
 );
