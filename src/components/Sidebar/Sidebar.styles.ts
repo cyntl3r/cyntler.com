c9b7fc47402ae7f2407a6590955bd2d02450edb2
cyntler <damian@cyntler.com>
2021-01-26 14:10:14 +0100

    Update Sidebar

--- src/components/Sidebar/Sidebar.styles.ts
+++ src/components/Sidebar/Sidebar.styles.ts
@@ -2,12 +2,31 @@
  * @name personal-frontend
  * @author cyntler <damian@cyntler.com>
  */
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
 
-export const Wrapper = styled.div`
+interface WrapperProps {
+  isActive: boolean;
+}
+
+export const Wrapper = styled.aside<WrapperProps>`
   position: fixed;
-  width: 370px;
-  left: 0;
+  width: 100vw;
+  height: 100vh;
+  left: -100vw;
   top: 0;
   font-size: 1.5rem;
+  background-color: ${({ theme }) => theme?.colors?.white};
+  transition: left 0.5s ease;
+
+  ${({ isActive }) =>
+    isActive &&
+    css`
+      left: 0;
+    `}
+
+  ${media.greaterThan('medium')`
+    width: 320px;
+    left: 0;
+  `}
 `;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,26 +2,49 @@
  * @name personal-frontend
  * @author cyntler <damian@cyntler.com>
  */
-import { FunctionComponent } from 'react';
-import { Link } from 'react-router-dom';
+import { FunctionComponent, useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { PaddingWrapper } from '../App/App.styles';
 import { Logomark } from '../Logomark/Logomark';
-import { PaddingWrapper } from '../PaddingWrapper/PaddingWrapper';
-import { SidebarBio } from '../SidebarBio/SidebarBio';
-import { SidebarFooter } from '../SidebarFooter/SidebarFooter';
-import { SidebarNavigation } from '../SidebarNavigation/SidebarNavigation';
-import { SidebarSocialMedia } from '../SidebarSocialMedia/SidebarSocialMedia';
+import { SidebarBio } from './SidebarBio/SidebarBio';
+import { SidebarFooter } from './SidebarFooter/SidebarFooter';
+import { SidebarMobileOpener } from './SidebarMobileOpener/SidebarMobileOpener';
+import { SidebarNavigation } from './SidebarNavigation/SidebarNavigation';
+import { SidebarSocialMedia } from './SidebarSocialMedia/SidebarSocialMedia';
 import { Wrapper } from './Sidebar.styles';
 
-export const Sidebar: FunctionComponent = () => (
-  <Wrapper>
-    <PaddingWrapper>
-      <Link to="/">
-        <Logomark />
-      </Link>
-      <SidebarBio />
-      <SidebarNavigation />
-      <SidebarSocialMedia />
-      <SidebarFooter />
-    </PaddingWrapper>
-  </Wrapper>
-);
+export const Sidebar: FunctionComponent = () => {
+  const location = useLocation();
+  const [isSidebarActive, setIsSidebarActive] = useState(false);
+
+  const handleSidebarOpen = () => {
+    setIsSidebarActive(true);
+  };
+
+  const handleSidebarClose = () => {
+    setIsSidebarActive(false);
+  };
+
+  useEffect(() => {
+    if (isSidebarActive) {
+      handleSidebarClose();
+    }
+  }, [location]);
+
+  return (
+    <>
+      <SidebarMobileOpener onClick={handleSidebarOpen} />
+      <Wrapper isActive={isSidebarActive}>
+        <PaddingWrapper>
+          <Link to="/">
+            <Logomark />
+          </Link>
+          <SidebarBio />
+          <SidebarNavigation />
+          <SidebarSocialMedia />
+          <SidebarFooter />
+        </PaddingWrapper>
+      </Wrapper>
+    </>
+  );
+};
