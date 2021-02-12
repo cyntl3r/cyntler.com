f0595c7f1d9228933fc0496e7259c2ff62e040f6
cyntler <damian@cyntler.com>
2021-02-12 14:01:05 +0100

    Add using Providers

--- src/index.tsx
+++ src/index.tsx
@@ -2,35 +2,16 @@
  * @name personal-frontend
  * @author cyntler <damian@cyntler.com>
  */
-import { StrictMode } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { App } from './components/App/App';
-import { GlobalStyles } from './components/App/App.styles';
-import { renderer } from './utils/renderer';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { LocaleContextProvider } from './contexts/localeContext/localeContext';
-import { theme } from './theme';
-import { IntlProvider } from './components/IntlProvider/IntlProvider';
-import { ApolloProvider } from '@apollo/client';
-import { apolloClient } from './apolloClient';
+import { renderer } from '~/utils/renderer';
+import { Providers } from '~/components/Providers/Providers';
+import { App } from '~/components/App/App';
 
 const appElement = document.querySelector('main');
 
 renderer(
-  <StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <LocaleContextProvider>
-        <Router>
-          <IntlProvider>
-            <ThemeProvider theme={theme}>
-              <GlobalStyles />
-              <App />
-            </ThemeProvider>
-          </IntlProvider>
-        </Router>
-      </LocaleContextProvider>
-    </ApolloProvider>
-  </StrictMode>,
+  <Providers>
+    <App />
+  </Providers>,
   appElement
 );
 
