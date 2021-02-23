a9bfb5586a9efc68ebe398d1bb57ebfeeeda1646
cyntler <damian@cyntler.com>
2021-02-23 23:56:29 +0100

    Add alias for react to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      react: join(__dirname, '/node_modules/react'),
+    },
   },
   module: {
     rules: [
@@ -32,9 +35,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       inject: 'body',
-      templateParameters: {
-        title: process.env.APP_TITLE,
-      },
     }),
     new CopyPlugin({
       patterns: [{ from: join(__dirname, 'public'), to: './' }],
