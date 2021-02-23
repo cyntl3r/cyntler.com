542925c7213a555d62675aa64a6aef1f65668f82
cyntler <damian@cyntler.com>
2021-02-23 23:57:13 +0100

    Move theme.ts to utils

---
+++ src/utils/theme.ts
@@ -0,0 +1,11 @@
+/**
+ * @name personal-frontend
+ * @author cyntler <damian@cyntler.com>
+ */
+export const theme = {
+  colors: {
+    black: '#000000',
+    white: '#ffffff',
+    silverChalice: '#a2a2a2',
+  },
+};
