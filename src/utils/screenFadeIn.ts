2c6d2d7c23f83193c84799fd11b02f090e9d2c59
cyntler <damian@cyntler.com>
2021-03-02 14:59:29 +0100

    Add screenFadeIn util

---
+++ src/utils/screenFadeIn.ts
@@ -0,0 +1,11 @@
+/**
+ * @name personal-frontend
+ * @author cyntler <damian@cyntler.com>
+ */
+export const screenFadeIn = (el: HTMLElement) => {
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      el.classList.add('loaded');
+    }, 200);
+  });
+};
