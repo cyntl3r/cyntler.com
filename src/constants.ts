4f334ba28b2e0efff938e7a8eefa4c62fd186e93
cyntler <damian@cyntler.com>
2021-03-03 15:24:47 +0100

    Update index.tsx

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @name personal-frontend
- * @author cyntler <damian@cyntler.com>
- */
-export const LOCALE_STORAGE_KEY = 'locale';
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@
  * @name personal-frontend
  * @author cyntler <damian@cyntler.com>
  */
-import { renderer } from '~/utils/renderer';
-import { Providers } from '~/components/Providers/Providers';
-import { App } from '~/components/App/App';
+import { renderer } from './utils/renderer';
+import { Providers } from './components/Providers/Providers';
+import { App } from './components/App/App';
+import { screenFadeIn } from './utils/screenFadeIn';
 
 const appElement = document.querySelector('main');
+
+screenFadeIn(appElement);
 
 renderer(
   <Providers>
@@ -14,7 +17,3 @@ renderer(
   </Providers>,
   appElement
 );
-
-window.addEventListener('load', () => {
-  appElement.classList.add('loaded');
-});
