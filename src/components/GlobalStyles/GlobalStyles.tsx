b6476b1842655e5ae5479fdc6430e55c54041329
cyntler <damian@cyntler.com>
2021-03-08 23:58:30 +0100

    Add GlobalStyles in separate component

---
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -0,0 +1,55 @@
+/**
+ * @name personal-frontend
+ * @author cyntler <damian@cyntler.com>
+ */
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: transparent;
+  }
+  
+  html,
+  body,
+  main {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme?.colors?.white};
+    font-family: 'Lato', sans-serif;
+    font-size: 1.2rem;
+  }
+
+  main {
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity 0.4s ease, visibility 0.4s ease;
+
+    &.loaded {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+    padding: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+`;
