9626ae4cc5b4c60706c5f50e784aa3861f809891
cyntler <damian@cyntler.com>
2021-03-14 22:14:23 +0100

    Update Layout component

---
+++ src/components/GlobalStyles/Layout.tsx
@@ -0,0 +1,9 @@
+/**
+ * @name personal-frontend
+ * @author cyntler <damian@cyntler.com>
+ */
+import styled from 'styled-components';
+
+export const PaddingWrapper = styled.div`
+  padding: 2rem;
+`;
