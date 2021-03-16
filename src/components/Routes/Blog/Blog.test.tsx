615bc70d1be90a927785cf5435a600c4304b27bc
cyntler <damian@cyntler.com>
2021-03-16 23:42:30 +0100

    Update Blog page

---
+++ src/components/Routes/Blog/Blog.test.tsx
@@ -0,0 +1,16 @@
+/**
+ * @name personal-frontend
+ * @author cyntler <damian@cyntler.com>
+ */
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import { Blog } from './Blog';
+
+afterEach(() => {
+  cleanup();
+});
+
+test('is wrapper', () => {
+  const { getByText } = render(<Blog />);
+  expect(getByText('')).toBeDefined();
+});
