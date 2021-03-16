be1270a3e4bd16c82d86f5fd7141a004f306935f
cyntler <damian@cyntler.com>
2021-03-16 23:42:05 +0100

    Update Home page

---
+++ src/components/Routes/Home/Home.test.tsx
@@ -0,0 +1,16 @@
+/**
+ * @name personal-frontend
+ * @author cyntler <damian@cyntler.com>
+ */
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import { Home } from './Home';
+
+afterEach(() => {
+  cleanup();
+});
+
+test('is wrapper', () => {
+  const { getByText } = render(<Home />);
+  expect(getByText('')).toBeDefined();
+});
