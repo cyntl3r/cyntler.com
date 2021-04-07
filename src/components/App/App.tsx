9a25b4ece98689e6210cd05d2f1aeb7f3993d57f
cyntler <damian@cyntler.com>
2021-04-08 00:21:19 +0200

    Fix routes render

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,27 +4,20 @@
  */
 import { FunctionComponent, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { useWindowBlurChangeTitle } from 'use-window-blur-change-title';
 import { Sidebar } from '../Sidebar/Sidebar';
 import { routes } from '../routes';
-import { useTranslation } from '../../hooks/useTranslation/useTranslation';
 
-export const App: FunctionComponent = () => {
-  const { t } = useTranslation();
-  useWindowBlurChangeTitle(t('blur_document_title'));
-
-  return (
-    <>
-      <Sidebar />
-      <Suspense fallback={null}>
-        <Switch>
-          {routes.map(({ exact, path, children }) => (
-            <Route key={path} exact={exact} path={path}>
-              {children}
-            </Route>
-          ))}
-        </Switch>
-      </Suspense>
-    </>
-  );
-};
+export const App: FunctionComponent = () => (
+  <>
+    <Sidebar />
+    <Suspense fallback={null}>
+      <Switch>
+        {routes.map(({ path, exact, children }) => (
+          <Route key={path} path={path} exact={exact}>
+            {children}
+          </Route>
+        ))}
+      </Switch>
+    </Suspense>
+  </>
+);
