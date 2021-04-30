c5d494516527e2b8a5f72a1ad236ad53b29b7415
cyntler <damian@cyntler.com>
2021-04-30 21:26:28 +0200

    Update MobileHeader styles

--- src/components/MobileHeader/MobileHeader.styles.ts
+++ src/components/MobileHeader/MobileHeader.styles.ts
@@ -3,7 +3,12 @@
  * @author cyntler <damian@cyntler.com>
  */
 import styled from 'styled-components';
+import { mediaScreenMin } from '../../utils/mediaScreen';
 
 export const MobileHeaderContainer = styled.header`
   padding: 1rem;
+
+  ${mediaScreenMin('medium')} {
+    display: none;
+  }
 `;
