0f021de6c5cb8460e4ef589e997974d33020e931
cyntler <damian@cyntler.com>
2021-05-04 21:00:30 +0200

    Remove old Layout component

--- src/components/GlobalStyles/Layout.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * @name personal-frontend
- * @author cyntler <damian@cyntler.com>
- */
-import styled from 'styled-components';
-
-export const PaddingWrapper = styled.div`
-  padding: 2rem;
-`;
